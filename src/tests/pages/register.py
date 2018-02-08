2c8970ba31e4e966ccad82f11ca0c5fffd9f9ad6
Charles Moga <charles.moga@digital.hmrc.gov.uk>
2018-02-08 08:59:12 +0000

    refactored registration tests to move more logic to page object

--- src/tests/pages/register.py
+++ src/tests/pages/register.py
@@ -2,10 +2,32 @@ from page_objects import PageObject, PageElement, MultiPageElement
 from selenium import webdriver
 
 class RegistrationPage(PageObject):
-    email      = PageElement(name="email")
-    email2     = PageElement(name="email2")
-    password1  = PageElement(name="password1")
-    password2  = PageElement(name="password2")
-    terms      = PageElement(id_="id_terms_agreed")
-    button     = PageElement(css="#signup_form > button")
-    error      = PageElement(css="#signup_form > li.input-field-container.has-error > ul > li:nth-child(1)")
+    registration_page_url   = 'https://sso.trade.great.gov.uk/accounts/signup/?next=https://www.great.gov.uk/triage/'
+    confirmation_page_url   = 'https://sso.trade.great.gov.uk/accounts/confirm-email/'
+    email                   = PageElement(name="email")
+    confirm_email           = PageElement(name="email2")
+    password                = PageElement(name="password1")
+    confirm_password        = PageElement(name="password2")
+    terms_and_conditions    = PageElement(id_="id_terms_agreed")
+    register_button         = PageElement(css="#signup_form > button")
+    registration_page_title = 'Register'
+    confirmation_page_title = 'Verify your email address'
+
+    def fill_registration_form(self,email1,email2,password1,password2, driver):
+        self.email              = email1
+        self.confirm_email      = email2
+        self.password           = password1
+        self.confirm_password   = password2
+        driver.execute_script("window.scrollTo(0, 1200)") #make register button and terms checkbox visible
+        self.terms_and_conditions.click()
+        self.register_button.click()
+
+    def get_title(self,driver):
+        return driver.find_element_by_css_selector('#profile-register-intro > h1').text
+
+    def get_confirmation_page_title(self,driver):
+        return driver.find_element_by_css_selector('#content > div > div > h1').text
+
+    def get_error(self,driver):
+        return driver.find_element_by_css_selector('#signup_form > li.input-field-container.has-error > ul > li:nth-child(1)').text
+
--- src/tests/steps/register.py
+++ src/tests/steps/register.py
@@ -1,9 +1,7 @@
 from behave import *
-from ...utils import config
 from ...pages import register
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
 
 chrome_options = Options()
 chrome_options.add_argument("--window-size=1920,1200")
@@ -13,60 +11,34 @@ driver.implicitly_wait(15)
 page = register.RegistrationPage(driver)
 @given('I have navigated to the registration page')
 def step_impl(context):
-    url = 'https://sso.trade.great.gov.uk/accounts/signup/?next=https://www.great.gov.uk/triage/'
-    driver.get(url)
-    title = driver.find_element_by_css_selector('#profile-register-intro > h1')
-    assert title.text == 'Register'
+    driver.get(page.registration_page_url)
+    assert page.get_title(driver) == page.registration_page_title
 
 @when('I register with {email} and {password}')
 def step_impl(context, email, password):
-    page.email = email
-    page.email2 = email
-    page.password1 = password
-    page.password2 = password
-    driver.execute_script("window.scrollTo(0, 1200)") #bring button to view
-    page.terms.click()
-    page.button.click()
+    page.fill_registration_form(email,email,password,password,driver)
 
 @then('I should see a confirmation message')
 def step_impl(context):
-    url = 'https://sso.trade.great.gov.uk/accounts/confirm-email/'
-    assert driver.current_url == url
-    title = driver.find_element_by_css_selector('#content > div > div > h1')
-    assert title.text == 'Verify your email address'
+    assert page.get_confirmation_page_title(driver) == page.confirmation_page_title
 
 @when('I register with {email},{password},{password2}')
 def step_impl(context,email,password,password2):
-    page.email = email
-    page.email2 = email
-    page.password1 = password
-    page.password2 = password2
-
-    driver.execute_script("window.scrollTo(0, 1200)") #make register button and terms checkbox visible
-    page.terms.click()
-    page.button.click()
+    page.fill_registration_form(email,email,password,password2,driver)
 
 @then('I should see error {error}')
 def step_impl(context, error):
-    assert page.error.text == error
+    assert page.get_error(driver) == error
 
 @when('I register with {email},{email2}')
 def step_impl(context,email,email2):
-    page.email = email
-    page.email2 = email2
-    page.password1 = 'assignmenT18'
-    page.password2 = 'assignmenT18'
-
-    driver.execute_script("window.scrollTo(0, 1200)") #make register button and terms checkbox visible
-    page.terms.click()
-    page.button.click()
+    page.fill_registration_form(email,email2,'assignmenT18','assignmenT18',driver)
 
 @then('the form will not be submitted')
 def step_impl(context):
-    url = 'https://sso.trade.great.gov.uk/accounts/signup/?next=https://www.great.gov.uk/triage/'
-    assert driver.current_url == url
+    assert page.get_title(driver) == page.registration_page_title
 
 @then('I should see the error {error} displayed')
 def step_impl(context, error):
-    assert page.error.text == error
+    assert page.get_error(driver) == error
     driver.quit()
